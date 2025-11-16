36e33c12b10a588321f40849d742b3aba2882cdc
Xhurian Shaba <111800311+IAlphaOmegaI@users.noreply.github.com>
2025-11-16 02:30:37 +0200

    chore: add linting command and refactor page layout for improved readability
    - Added a new lint command to package.json for code quality checks.
    - Refactored the layout of the page component to enhance readability and maintainability, including adjustments to class names and structure.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,14 @@
-import { Button } from "@zenncore/web/components/button";
+import { ArrowRightIcon } from "@zenncore/icons";
 import Image from "next/image";
+import Background from "@/public/images/background.png";
 import Banner from "@/public/images/banner.png";
 import Logo from "@/public/images/logo.png";
-import Background from "@/public/images/background.png";
-import { ArrowRightIcon } from "@zenncore/icons";
 
 export default () => {
   return (
-    <main className="size-full items-center md:p-12 p-6 relative flex flex-col gap-12 pb-0 max-h-[100dvh] mb-auto">
+    <main className="relative mb-auto flex size-full max-h-[100dvh] flex-col items-center gap-12 p-6 pb-0 md:p-12">
       <div
-        className="absolute inset-0 size-full opacity-50 -z-30"
+        className="-z-30 absolute inset-0 size-full opacity-50"
         id="title/background"
         style={{
           backgroundImage: `repeating-linear-gradient(
@@ -29,38 +28,38 @@ export default () => {
         src={Background}
         alt="Logo"
         width={1000}
-        className="absolute top-0 -z-10 -translate-y-1/2 left-1/2 aspect-auto w-screen max-w-[1000px] -translate-x-1/2"
+        className="-z-10 -translate-y-1/2 -translate-x-1/2 absolute top-0 left-1/2 aspect-auto w-screen max-w-[1000px]"
       />
-      <div className="flex flex-col gap-1 text-white w-fit mx-auto relative">
-        <h1 className="text-5xl md:text-6xl font-semibold  tracking-tight flex items-center gap-2">
+      <div className="relative mx-auto flex w-fit flex-col gap-1 text-white">
+        <h1 className="flex items-center gap-2 font-semibold text-5xl tracking-tight md:text-6xl">
           <Image src={Logo} alt="Logo" width={140} height={100} /> Instincts
         </h1>
-        <h3 className="font-body text-xl text-center font-bold md:leading-2 leading-1 pl-1 tracking-tight">
+        <h3 className="pl-1 text-center font-body font-bold text-xl leading-1 tracking-tight md:leading-2">
           What if Wolt could read your mind?
         </h3>
       </div>
-      <section className="flex-1 relative w-full flex items-end justify-center text-white">
+      <section className="relative flex w-full flex-1 items-end justify-center text-white">
         <Image
           src={Banner}
           alt="Banner"
           blurDataURL={Banner.blurDataURL}
-          className="h-full w-auto absolute -z-20 aspect-auto"
+          className="-z-20 absolute aspect-auto h-full w-auto"
         />
-        <div className="h-full bg-gradient-to-t from-background-dimmed to-background-dimmed/0 absolute bottom-0 inset-x-0 -z-10" />
+        <div className="-z-10 absolute inset-x-0 bottom-0 h-full bg-gradient-to-t from-background-dimmed to-background-dimmed/0" />
 
-        <div className="flex flex-col gap-4 items-center md:p-12 p-6 text-white">
-          <h1 className="font-semibold flex items-center gap-2 md:text-7xl sm:text-5xl text-4xl text-center">
+        <div className="flex flex-col items-center gap-4 p-6 text-white md:p-12">
+          <h1 className="flex items-center gap-2 text-center font-semibold text-4xl sm:text-5xl md:text-7xl">
             <Image src={Logo} alt="Logo" width={140} height={100} /> Instincts
           </h1>
-          <p className="md:text-xl sm:text-base text-sm text-center font-medium">
+          <p className="text-center font-medium text-sm sm:text-base md:text-xl">
             Experience the future of food delivery, install the app and let us
             know what you think.
           </p>
           <button
             type="button"
-            className="rounded-full border border-white flex gap-4 items-center p-0.5 text-white bg-primary font-semibold px-4 py-2 flex items-center justify-center hover:bg-primary-rich transition-all duration-300 active:scale-95"
+            className="flex flex items-center items-center justify-center gap-4 rounded-full border border-white bg-primary p-0.5 px-4 py-2 font-semibold text-white transition-all duration-300 hover:bg-primary-rich active:scale-95"
           >
-            Install App <ArrowRightIcon className="w-4 h-4 fill-white" />
+            Install App <ArrowRightIcon className="h-4 w-4 fill-white" />
           </button>
         </div>
       </section>
